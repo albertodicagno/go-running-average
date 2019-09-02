7292c91debdeb9c7f51befb6e5b61d2137dfae10
Alberto Di Cagno <albertodicagno@gmail.com>
2019-09-02 14:53:48 +0200

    Basic test def

--- running_average_test.go
+++ running_average_test.go
@@ -1,1 +1,22 @@
-package main_test
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAverage(t *testing.T) {
+	ravg := NewRunningAverage(8)
+	ravg.AddSample(1)
+	ravg.AddSample(2)
+	ravg.AddSample(3)
+	ravg.AddSample(4)
+	ravg.AddSample(1)
+	ravg.AddSample(2)
+	ravg.AddSample(3)
+	ravg.AddSample(4)
+
+	_, err := ravg.GetAverage()
+	assert.Nil(t, err)
+}
