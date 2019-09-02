63e1d9a3cbde93e52d18a13d4f2023a39d1f12ff
Alberto Di Cagno <albertodicagno@gmail.com>
2019-09-02 15:05:30 +0200

    Basic test for Clear()

--- running_average_test.go
+++ running_average_test.go
@@ -6,8 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var ravg *RunningAverage
+
 func TestGetAverage(t *testing.T) {
-	ravg := NewRunningAverage(8)
+	ravg = NewRunningAverage(8)
 	ravg.AddSample(1)
 	ravg.AddSample(2)
 	ravg.AddSample(3)
@@ -19,4 +21,12 @@ func TestGetAverage(t *testing.T) {
 
 	_, err := ravg.GetAverage()
 	assert.Nil(t, err)
+}
+
+func TestClear(t *testing.T) {
+	ravg.Clear()
+	assert.Equal(t, 0, ravg.Samples[4])
+	assert.Equal(t, 0, ravg.Min)
+	assert.Equal(t, 0, ravg.Max)
+	assert.Equal(t, 0, ravg.Average)
 }
