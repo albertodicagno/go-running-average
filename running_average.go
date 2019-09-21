5ed77fb8cc9c7cee03e749585d20c8a5ff3a5b7a
Alberto Di Cagno <a.dicagno@controlconsulting.it>
2019-09-21 08:57:16 +0200

    Added comments for godoc

--- running_average.go
+++ running_average.go
@@ -5,11 +5,7 @@ import (
 	"math"
 )
 
-<<<<<<< HEAD
-//RunningAverage structure holding average and other values
-=======
 //RunningAverage defines the data structure holding samples, min/max and average values, along with internal values and counters
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 type RunningAverage struct {
 	SampleCount uint
 	Samples     []float64
@@ -21,11 +17,7 @@ type RunningAverage struct {
 	sum         float64
 }
 
-<<<<<<< HEAD
-//NewRunningAverage allocates memory and returns a pointer for a RunningAverage object with numSamples samples
-=======
 //NewRunningAverage returns a pointer to a new RunningAverage object with sample size of numSamples
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 func NewRunningAverage(numSamples uint) *RunningAverage {
 	avg := new(RunningAverage)
 	avg.SampleCount = numSamples
@@ -33,11 +25,7 @@ func NewRunningAverage(numSamples uint) *RunningAverage {
 	return avg
 }
 
-<<<<<<< HEAD
-//AddSample adds a new sample of type float64 to the RunningAverage buffer
-=======
 //AddSample adds a new sample to the RunningAverage struct
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 func (avg *RunningAverage) AddSample(sample float64) {
 	avg.sum -= avg.Samples[avg.cursor]
 	avg.Samples[avg.cursor] = sample
@@ -62,11 +50,7 @@ func (avg *RunningAverage) AddSample(sample float64) {
 	}
 }
 
-<<<<<<< HEAD
-//Clear resets the average
-=======
 //Clear resets the whole RunningAverage struct
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 func (avg *RunningAverage) Clear() {
 	avg.Min = 0.00
 	avg.Max = 0.00
@@ -79,11 +63,7 @@ func (avg *RunningAverage) Clear() {
 	}
 }
 
-<<<<<<< HEAD
-//Fill fills buffer with specified sample
-=======
 //Fill fills the sample buffer with the value passed as an argument
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 func (avg *RunningAverage) Fill(sample float64) {
 	avg.Clear()
 	for i := uint(0); i < avg.SampleCount; i++ {
@@ -91,11 +71,7 @@ func (avg *RunningAverage) Fill(sample float64) {
 	}
 }
 
-<<<<<<< HEAD
-//GetAverage returns computed average iterating through the sample buffer
-=======
 //GetAverage computes and returns the average iterating across the whole sample buffer
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 func (avg *RunningAverage) GetAverage() (float64, error) {
 	if avg.counter == 0 {
 		return -1, errors.New("average is not yet ready")
@@ -109,11 +85,7 @@ func (avg *RunningAverage) GetAverage() (float64, error) {
 	return avg.sum / float64(avg.counter), nil
 }
 
-<<<<<<< HEAD
-//GetFastAverage returns the last computed average value without performing iterations
-=======
 //GetFastAverage same as GetAverage, but faster since no iteration is made, just using the last computed values for sum and sample count
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 func (avg *RunningAverage) GetFastAverage() (float64, error) {
 	if avg.counter == 0 {
 		return -1, errors.New("average is not yet ready")
@@ -122,11 +94,7 @@ func (avg *RunningAverage) GetFastAverage() (float64, error) {
 	return avg.sum / float64(avg.counter), nil
 }
 
-<<<<<<< HEAD
-//GetStandardDeviation computes and returns the stdev
-=======
 //GetStandardDeviation returns the stdev across the sample buffer
->>>>>>> e0aeaf130610e7e1b21376730e16e7e66be7756c
 func (avg *RunningAverage) GetStandardDeviation() (float64, error) {
 	if avg.counter == 0 {
 		return -1, errors.New("average is not ready")
